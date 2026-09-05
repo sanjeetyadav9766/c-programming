5d90192ac5c6679d67578547c27b8dc6294884f4
Sanjeet Subhash Singh Yadav <sanjeetyadav9766@gmail.com>
2026-09-05 14:52:24 +0530

    Create program-1.c

---
+++ unit-1/program-1.c
@@ -0,0 +1,11 @@
+
+// sum of number
+# include <stdio.h>
+int main()
+{
+    int num1,num2,sum;
+    printf ("enter the two number");
+    scanf ("%d %d", & num1, &num2);
+    printf ("sum=%d\n",num1+num2);
+    return 0;
+}
