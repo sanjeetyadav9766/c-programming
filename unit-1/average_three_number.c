042767b62b1ea49c94b3a6ec889b50228909cdfc
Sanjeet Subhash Singh Yadav <sanjeetyadav9766@gmail.com>
2026-09-12 20:04:11 +0530

    Add files via upload

---
+++ unit-1/average_three_number.c
@@ -0,0 +1,10 @@
+// calulate the average of three number
+# include <stdio.h>
+int main()
+{
+float a,b,c,average;
+printf("enter the three number");
+scanf ("%f%f%f", & a, &b, &c);
+printf("average=%2f\n",(a+b+c)/3);
+return 0;
+}
