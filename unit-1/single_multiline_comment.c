bc4941f3e11f44ba36ef217efde018a6aba22612
Sanjeet Subhash Singh Yadav <sanjeetyadav9766@gmail.com>
2026-09-13 18:16:59 +0530

    Add files via upload

---
+++ unit-1/single_multiline_comment.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+int main()
+{
+//this is single line comment
+printf("hello world!\n");
+/*this is multiline comment
+it can contain multiline
+*/
+printf("this program demonstrate comment in c \n");
+return 0;    
+}
